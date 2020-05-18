556bb8d5f6f719834885e4adc854eafc89164764
lewtun <lewis.c.tunstall@gmail.com>
2020-05-19 00:40:33 +0200

    Lesson06 (#6)
    * Add template for lesson06
    
    * Add 3D shapes completed sections
    
    * Add final notebook for lesson06
    
    * Add AGPLv3 licence and binder postbuild

--- hepml/_nbdev.py
+++ hepml/_nbdev.py
@@ -3,6 +3,8 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"download_dataset": "00_core.ipynb",
+         "make_point_clouds": "00_core.ipynb",
+         "load_shapes": "00_core.ipynb",
          "display_large": "00_core.ipynb",
          "rf_feature_importance": "00_core.ipynb",
          "plot_feature_importance": "00_core.ipynb",
