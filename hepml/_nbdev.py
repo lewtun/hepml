c97b5879e3e3c3edd06dea44464789028222c956
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2020-05-23 19:58:21 +0200

    FInalise notebook for lesson07

--- hepml/_nbdev.py
+++ hepml/_nbdev.py
@@ -5,6 +5,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"download_dataset": "00_core.ipynb",
          "make_point_clouds": "00_core.ipynb",
          "load_shapes": "00_core.ipynb",
+         "make_gravitational_waves": "00_core.ipynb",
          "display_large": "00_core.ipynb",
          "rf_feature_importance": "00_core.ipynb",
          "plot_feature_importance": "00_core.ipynb",
